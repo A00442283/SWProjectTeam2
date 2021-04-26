7c77002f1c828521927be290b70761970f0b6993
A00442283 <77292688+A00442283@users.noreply.github.com>
2021-04-26 15:49:10 -0300

    Update inclassContext.cs

--- Models/inclassContext.cs
+++ Models/inclassContext.cs
@@ -34,7 +34,7 @@ namespace eBikes1.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:5510him.database.windows.net,1433;Initial Catalog=inclass;Persist Security Info=False;User ID=himanshu;Password=Sharedpass123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=tcp:ebikesdbserver.database.windows.net,1433;Initial Catalog=inclass;Persist Security Info=False;User ID=maria;Password=Sharedpass123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
